675baa882e579369c58af773bec461c93724cff5
Sebastian <sebastian.messlinger@posteo.de>
2019-11-08 15:49:13 +0100

    started filling out template with memcopy

--- 3/inc/chCommandLine.h
+++ 3/inc/chCommandLine.h
@@ -40,13 +40,13 @@
 #include <stdlib.h>
 
 static void
-chCommandLinePassback( int *p, char *s )
+chCommandLinePassback(int* p, char* s)
 {
-    *p = atoi( s );
+    *p = atoi(s);
 }
 
 static void
-chCommandLinePassback( char **p, char *s )
+chCommandLinePassback(char** p, char* s)
 {
     *p = s;
 }
@@ -55,20 +55,19 @@ chCommandLinePassback( char **p, char *s )
 // Passes back an integer or string
 //
 template<typename T>
-static bool
-chCommandLineGet( T *p, const char *keyword, int argc, char *argv[] )
+static bool chCommandLineGet(T* p, const char* keyword, int argc, char* argv[])
 {
     bool ret = false;
-    for ( int i = 1; i < argc; i++ ) {
-        char *s = argv[i];
-        if ( *s == '-' ) {
+    for (int i = 1; i<argc; i++) {
+        char* s = argv[i];
+        if (*s=='-') {
             s++;
-            if ( *s == '-' ) {
+            if (*s=='-') {
                 s++;
             }
-            if ( ! strcmp( s, keyword ) ) {
-                if ( ++i <= argc ) {
-                    chCommandLinePassback( p, argv[i] );
+            if (!strcmp(s, keyword)) {
+                if (++i<=argc) {
+                    chCommandLinePassback(p, argv[i]);
                     ret = true;
                 }
             }
@@ -80,18 +79,17 @@ chCommandLineGet( T *p, const char *keyword, int argc, char *argv[] )
 //
 // Pass back true if the keyword is passed as a command line parameter
 //
-static bool
-chCommandLineGetBool( const char *keyword, int argc, char *argv[] )
+static bool chCommandLineGetBool(const char* keyword, int argc, char* argv[])
 {
     bool ret = false;
-    for ( int i = 1; i < argc; i++ ) {
-        char *s = argv[i];
-        if ( *s == '-' ) {
+    for (int i = 1; i<argc; i++) {
+        char* s = argv[i];
+        if (*s=='-') {
             s++;
-            if ( *s == '-' ) {
+            if (*s=='-') {
                 s++;
             }
-            if ( ! strcmp( s, keyword ) ) {
+            if (!strcmp(s, keyword)) {
                 return true;
             }
         }
--- 3/src/kernel.cu
+++ 3/src/kernel.cu
@@ -9,41 +9,40 @@
  *                         Purpose : Memory Operations Benchmark
  *
  *************************************************************************************************/
+#include <cuda_runtime.h>
 
 //
 // Kernels
 //
 
-__global__ void 
-globalMemCoalescedKernel(/*TODO Parameters*/)
+
+__global__ void globalMemCoalescedKernel(/*TODO Parameters*/)
 {
     /*TODO Kernel Code*/
 }
 
-void 
-globalMemCoalescedKernel_Wrapper(dim3 gridDim, dim3 blockDim /*TODO Parameters*/) {
-	globalMemCoalescedKernel<<< gridDim, blockDim, 0 /*Shared Memory Size*/ >>>( /*TODO Parameters*/);
+void globalMemCoalescedKernel_Wrapper(dim3 gridDim, dim3 blockDim /*TODO Parameters*/)
+{
+    globalMemCoalescedKernel <<<gridDim, blockDim, 0 /*Shared Memory Size*/ >>>( /*TODO Parameters*/);
 }
 
-__global__ void 
-globalMemStrideKernel(/*TODO Parameters*/)
+__global__ void globalMemStrideKernel(/*TODO Parameters*/)
 {
     /*TODO Kernel Code*/
 }
 
-void 
-globalMemStrideKernel_Wrapper(dim3 gridDim, dim3 blockDim /*TODO Parameters*/) {
-	globalMemStrideKernel<<< gridDim, blockDim, 0 /*Shared Memory Size*/ >>>( /*TODO Parameters*/);
+void globalMemStrideKernel_Wrapper(dim3 gridDim, dim3 blockDim /*TODO Parameters*/)
+{
+    globalMemStrideKernel <<<gridDim, blockDim, 0 /*Shared Memory Size*/ >>>( /*TODO Parameters*/);
 }
 
-__global__ void 
-globalMemOffsetKernel(/*TODO Parameters*/)
+__global__ void globalMemOffsetKernel(/*TODO Parameters*/)
 {
     /*TODO Kernel Code*/
 }
 
-void 
-globalMemOffsetKernel_Wrapper(dim3 gridDim, dim3 blockDim /*TODO Parameters*/) {
-	globalMemOffsetKernel<<< gridDim, blockDim, 0 /*Shared Memory Size*/ >>>( /*TODO Parameters*/);
+void globalMemOffsetKernel_Wrapper(dim3 gridDim, dim3 blockDim /*TODO Parameters*/)
+{
+    globalMemOffsetKernel <<<gridDim, blockDim, 0 /*Shared Memory Size*/ >>>( /*TODO Parameters*/);
 }
 
