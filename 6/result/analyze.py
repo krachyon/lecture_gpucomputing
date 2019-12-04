23e738e89a967087d07f31fff074fd446a5b9982
Sebastian <sebastian.messlinger@posteo.de>
2019-12-04 15:59:30 +0100

    blocksize diagrams

--- 6/result/analyze.py
+++ 6/result/analyze.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from cycler import cycler
 
 def calc_bandwidth(df):
     """given a data frame with expected format, add bandwidth in GiB/s to df"""
@@ -25,7 +27,7 @@ shared=dat[(dat.method=='cuda_shared')&(dat.dtype=='float')]
 
 
 def full_bw():
-    plt.figure()
+    plt.figure(figsize=(10,8))
     plt.loglog(naive.N_elem, naive.bw_full, 'x', label='cuda_global')
     plt.loglog(shared.N_elem, shared.bw_full, 'x', label='cuda_shared')
     plt.loglog(thrust.N_elem, thrust.bw_full, label='thrust::accumulate')
@@ -37,10 +39,11 @@ def full_bw():
     plt.xlabel("number of elements")
     plt.ylabel("bandwidth [GiB/s]")
     plt.legend()
+    plt.savefig('full_bw.pdf')
 
 
 def exec_bw():
-    plt.figure()
+    plt.figure(figsize=(10,8))
     plt.loglog(naive.N_elem, naive.bw_exec, 'x', label='cuda_global')
     plt.loglog(shared.N_elem, shared.bw_exec, 'x', label='cuda_shared')
     plt.loglog(thrust.N_elem, thrust.bw_exec, label='thrust::accumulate')
@@ -52,10 +55,37 @@ def exec_bw():
     plt.xlabel("number of elements")
     plt.ylabel("bandwidth [GiB/s]")
     plt.legend()
+    plt.savefig('exec_bw.pdf')
+
+def blocksize():
+    plt.rc('axes', prop_cycle=cycler(color=[cm.nipy_spectral(i/7) for i in range(7)]))
+    plt.figure(figsize=(10,8))
+    for bs in sorted(pd.unique(dat.N_block)):
+        naive_n = dat[(dat.method == 'cuda_naive') & (dat.dtype == 'float') & (dat.N_block == bs)]
+        plt.loglog(naive_n.N_elem, naive_n.bw_exec, 'x', label=f'naive bs={int(bs)}')
+
+    plt.title("blocksize dependent throughput, naive")
+    plt.xlabel("number of elements")
+    plt.ylabel("bandwidth [GiB/s]")
+    plt.legend()
+    plt.savefig('naive_bs.pdf')
+
+    plt.figure(figsize=(10,8))
+    for bs in sorted(pd.unique(dat.N_block)):
+        shared_n = dat[(dat.method == 'cuda_shared') & (dat.dtype == 'float') & (dat.N_block == bs)]
+        plt.loglog(shared_n.N_elem, shared_n.bw_exec, 'x', label=f'shared bs={int(bs)}')
+
+    plt.xlabel("blocksize dependent throughput, shared")
+    plt.ylabel("bandwidth [GiB/s]")
+    plt.legend()
+    plt.savefig('shared_bs.pdf')
+
+
 
 
 if __name__=='__main__':
     full_bw()
     exec_bw()
+    blocksize()
 
-    plt.show()
+    #plt.show()
