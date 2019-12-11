78076153fbed6020b8ce58ed6712801ab6770b56
Sebastian <sebastian.messlinger@posteo.de>
2019-12-11 09:38:39 +0100

    experiment with parallel cpu kernel

--- 6/reduction/reduction.cpp
+++ 6/reduction/reduction.cpp
@@ -1,11 +1,13 @@
 #include "reduction.h"
 #include "tracing.h"
+#include <future>
+#include <numeric>
+#include <iterator>
 
 float reduce_cpu(std::vector<float>const& in_orig)
 {
     auto in = in_orig;
 
-    unsigned long long int gauss = in.size()*(in.size()+1)/2;
 
 
     for(size_t stride=2;stride <= in.size();stride*=2){
@@ -24,4 +26,38 @@ float reduce_cpu_seq(std::vector<float>const& in)
         res+=elem;
 
     return res;
+}
+
+float reduce_cpu_async(std::vector<float>const& in)
+{
+    size_t n_threads = std::thread::hardware_concurrency();
+
+    auto acc = [](decltype(in.begin()) beg, decltype(in.end()) end){
+        return std::accumulate(beg, end, 0.0f);
+    };
+
+    using future = decltype(std::async(std::launch::async, acc, in.begin(), in.end()));
+
+    std::vector<future> fut;
+
+    size_t bs = in.size()/n_threads;
+
+    for(size_t i=0; i != n_threads; ++i)
+    {
+        fut.push_back( std::async(std::launch::async,
+                acc, in.begin()+i*bs, in.begin()+(i+1)*bs
+                ));
+    }
+    fut.push_back( std::async(std::launch::async,
+                              acc, in.begin()+ (n_threads*bs), in.end()
+    ));
+
+    auto ret = 0.0f;
+    for(auto& res: fut)
+    {
+        ret += res.get();
+    }
+
+    return ret;
+
 }
--- 6/reduction_test/test_cpu.cpp
+++ 6/reduction_test/test_cpu.cpp
@@ -1,37 +1,49 @@
 #include <gtest/gtest.h>
 #include "reduction.h"
 #include <numeric>
+//
+//TEST(cpu, ones)
+//{
+//    std::vector<float> in0(1,1);
+//    EXPECT_FLOAT_EQ(1, reduce_cpu(in0));
+//    std::vector<float> in(1000,1);
+//    EXPECT_FLOAT_EQ(1000, reduce_cpu(in));
+//    std::vector<float> in2(2000,1);
+//    EXPECT_FLOAT_EQ(2000, reduce_cpu(in2));
+//}
+//
+//TEST(cpu, debug)
+//{
+//    std::vector<float> in0(17,1);
+//    EXPECT_FLOAT_EQ(16, reduce_cpu(in0));
+//}
+//
+//TEST(cpu, zero)
+//{
+//    std::vector<float> in0(1,0);
+//    EXPECT_FLOAT_EQ(0, reduce_cpu(in0));
+//    std::vector<float> in(1000,0);
+//    EXPECT_FLOAT_EQ(0, reduce_cpu(in));
+//    std::vector<float> in2(2000,0);
+//    EXPECT_FLOAT_EQ(0, reduce_cpu(in2));
+//}
+//
+//TEST(cpu, iota)
+//{
+//    std::vector<float> in(1000);
+//    std::iota(in.begin(),in.end(),1);
+//    EXPECT_FLOAT_EQ(std::accumulate(in.begin(),in.end(),0.f), reduce_cpu(in));
+//}
 
-TEST(cpu, ones)
+TEST(cpu_async, ones)
 {
-    std::vector<float> in0(1,1);
-    EXPECT_FLOAT_EQ(1, reduce_cpu(in0));
-    std::vector<float> in(1000,1);
-    EXPECT_FLOAT_EQ(1000, reduce_cpu(in));
-    std::vector<float> in2(2000,1);
-    EXPECT_FLOAT_EQ(2000, reduce_cpu(in2));
-}
+//    std::vector<float> in0(1,1);
+//    EXPECT_FLOAT_EQ(1, reduce_cpu_async(in0));
+//    std::vector<float> in(1000,1);
+//    EXPECT_FLOAT_EQ(1000, reduce_cpu_async(in));
+//    std::vector<float> in2(2000,1);
+//    EXPECT_FLOAT_EQ(2000, reduce_cpu_async(in2));
 
-TEST(cpu, debug)
-{
-    std::vector<float> in0(17,1);
-    EXPECT_FLOAT_EQ(16, reduce_cpu(in0));
+    std::vector<float> in3(2000000000,1);
+    EXPECT_FLOAT_EQ(2000000000, reduce_cpu_async(in3));
 }
-
-TEST(cpu, zero)
-{
-    std::vector<float> in0(1,0);
-    EXPECT_FLOAT_EQ(0, reduce_cpu(in0));
-    std::vector<float> in(1000,0);
-    EXPECT_FLOAT_EQ(0, reduce_cpu(in));
-    std::vector<float> in2(2000,0);
-    EXPECT_FLOAT_EQ(0, reduce_cpu(in2));
-}
-
-TEST(cpu, iota)
-{
-    std::vector<float> in(1000);
-    std::iota(in.begin(),in.end(),1);
-    EXPECT_FLOAT_EQ(std::accumulate(in.begin(),in.end(),0.f), reduce_cpu(in));
-}
-
