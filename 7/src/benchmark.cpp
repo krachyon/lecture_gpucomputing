247736e09e51ff905d467df7159adb3f9bfec830
Sebastian <sebastian.messlinger@posteo.de>
2019-12-25 23:19:05 +0100

    benchmark parameters

--- 7/src/benchmark.cpp
+++ 7/src/benchmark.cpp
@@ -3,8 +3,8 @@
 
 
 static void CustomArguments(benchmark::internal::Benchmark* b) {
-    for (int n = 64; n <= 1024; n*=2)
-        for (int thread : {32, 1024})
+    for (int n = 128; n <= 1024; n+=128)
+        for (int thread : {1,2,8,16,32,50,128,200,512,700})
             b->Args({n, thread});
 }
 
@@ -20,6 +20,8 @@ static void BM_nbody_naive(benchmark::State& state)
 
         state.SetIterationTime(time.count());
     }
+    state.counters["N"] = N;
+    state.counters["threads_per_block"] = threads_per_block;
 }
 static void BM_nbody_naive_unaligned(benchmark::State& state)
 {
@@ -31,9 +33,28 @@ static void BM_nbody_naive_unaligned(benchmark::State& state)
         auto time = run_leapfrog_aos_unaligned(N, threads_per_block, iters).first;
 
         state.SetIterationTime(time.count());
-    }}
+    }
+    state.counters["N"] = N;
+    state.counters["threads_per_block"] = threads_per_block;
+}
+
+static void BM_nbody_shared(benchmark::State& state)
+{
+    size_t N = state.range(0);
+    size_t threads_per_block = state.range(1);
+
+    for (auto _ : state) {
+
+    auto time = run_leapfrog_soa(N, threads_per_block, iters).first;
+
+    state.SetIterationTime(time.count());
+    }
+    state.counters["N"] = N;
+    state.counters["threads_per_block"] = threads_per_block;
+}
 
 BENCHMARK(BM_nbody_naive)->Apply(CustomArguments)->UseManualTime()->Unit(benchmark::kMillisecond);
 BENCHMARK(BM_nbody_naive_unaligned)->Apply(CustomArguments)->UseManualTime()->Unit(benchmark::kMillisecond);;
+BENCHMARK(BM_nbody_shared)->Apply(CustomArguments)->UseManualTime()->Unit(benchmark::kMillisecond);;
 
 BENCHMARK_MAIN();
